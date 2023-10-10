5b2c66de02684004105158e352b890485d2024db
giovanaFurlan <giovananelofurlan@gmail.com>
2023-10-10 16:43:58 -0300

    fix: Ajustes front

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,27 +1,42 @@
 import { useState } from 'react';
 import {
-  Box,
   Button,
   Container,
   Flex,
   FormControl,
   FormLabel,
+  Grid,
+  GridItem,
   Input,
   InputGroup,
   InputLeftElement,
   InputRightElement,
-  List,
-  ListItem,
+  Tag,
+  TagCloseButton,
+  TagLabel,
   Text,
   Textarea,
+  useColorModeValue,
   useToast
 } from '@chakra-ui/react'
 import { FaRobot } from 'react-icons/fa';
+import { BsSlashLg } from "react-icons/bs";
 import { BiCopy } from 'react-icons/bi';
+import { MdHttp } from "react-icons/md";
 import Script from 'next/script';
 import DualListBox from "react-dual-listbox";
 import "react-dual-listbox/lib/react-dual-listbox.css";
 import CopyToClipboard from 'react-copy-to-clipboard';
+import styled from 'styled-components';
+
+const Style = styled.div`
+.react-dual-listbox select{
+  min-height: 300px;
+}
+input{
+  border-radius: 6px;
+}
+`
 
 const options = [
   {
@@ -100,7 +115,10 @@ const options = [
 
 export default function New() {
 
-  const [path, setPath] = useState('/');
+  const bg = useColorModeValue("cinzaClaro", "gray.800");
+  const color = useColorModeValue("black", "white");
+
+  const [path, setPath] = useState('');
   const [pathsArray, setPathsArray] = useState([]);
 
   const [sitemap, setSiteMap] = useState();
@@ -109,17 +127,16 @@ export default function New() {
 
   const toast = useToast();
 
-  /////////////////////
-
-  const inputSubmit = (e) => {
+  const handleInputSubmit = (e) => {
     e.preventDefault();
-    if (path.includes("/")) {
-      setPathsArray([...pathsArray, path]);
+    if (path != null) {
+      const newPaths = path.split(",").map(item => item.trim());
+      setPathsArray([...pathsArray, ...newPaths]);
       setBlockeds([...blockeds, []]);
-      setPath("/");
+      setPath("");
     } else {
       toast({
-        title: 'Add / in the beggining of the path',
+        title: 'camposObrigatorios',
         status: 'error',
         duration: 5000,
         position: "top",
@@ -141,38 +158,48 @@ export default function New() {
     });
   };
 
-  const generateResult = () => {
-    const agentRules = {};
+  const handleResult = () => {
 
-    pathsArray.forEach((path, index) => {
-      const blockedList = blockeds[index];
-      const allowedList = options.filter(option => !blockedList.includes(option.value));
+    if (sitemap != null) {
+      const agentRules = {};
 
-      options.forEach(option => {
-        const agentName = option.label;
-        if (blockedList.includes(option.value)) {
-          agentRules[agentName] = agentRules[agentName] || [];
-          agentRules[agentName].push(`Disallow: ${path}`);
-        } else {
-          agentRules[agentName] = agentRules[agentName] || [];
-          agentRules[agentName].push(`Allow: ${path}`);
-        }
+      pathsArray.forEach((path, index) => {
+        const blockedList = blockeds[index] || []; // Certifique-se de que blockedList está definido
+
+        options.forEach(option => {
+          const agentName = option.label;
+          if (blockedList.includes(option.value)) {
+            agentRules[agentName] = agentRules[agentName] || [];
+            agentRules[agentName].push(`Disallow: ${path}`);
+          } else {
+            agentRules[agentName] = agentRules[agentName] || [];
+            agentRules[agentName].push(`Allow: ${path}`);
+          }
+        });
       });
-    });
 
-    const finalResult = Object.entries(agentRules).map(([agent, rules]) => {
-      return `User-agent: ${agent}\n${rules.join('\n')}`;
-    });
+      const finalResult = Object.entries(agentRules).map(([agent, rules]) => {
+        return `User-agent: ${agent}\n${rules.join('\n')}`;
+      });
 
-    const result = finalResult.concat(" ", `Sitemap: ${sitemap}`);
+      const result = finalResult.concat(" ", `User-agent: * \nAllow: / \n\nSitemap: ${sitemap}`);
 
-    const textoFormatado = result.toString().replace(/,/g, ',\n').replaceAll(",", '');
-    setResults(textoFormatado);
+      const textoFormatado = result.toString().replace(/,/g, ',\n').replaceAll(",", '');
+      setResults(textoFormatado);
 
+    } else {
+      toast({
+        title: 'camposObrigatorios',
+        status: 'error',
+        duration: 5000,
+        position: "top",
+        isClosable: true,
+      })
+    }
   };
 
   return (
-    <>
+    <Style>
       <Script src="https://kit.fontawesome.com/08df0b650e.js" crossOrigin="anonymous"></Script>
       <Container
         maxW={"6xl"}
@@ -181,24 +208,50 @@ export default function New() {
         py={6}
         gap={6}>
         <form
-          onSubmit={inputSubmit}
-          style={{
-            display: "flex",
-            alignItems: "center",
-            gap: 4
-          }}>
-          <FormLabel>Páginas</FormLabel>
-          <Input type="text" value={path} onChange={(e) => setPath(e.target.value)} placeholder="Enter a path" />
-          <Button type="submit">Adicionar</Button>
+          onSubmit={handleInputSubmit}>
+          <FormControl isRequired>
+            <FormLabel
+              variant={"h6"}
+              color={"cinza"}>
+              Páginas
+            </FormLabel>
+            <Flex gap="2">
+              <InputGroup>
+                <InputLeftElement
+                  pointerEvents="none"
+                  h="full"
+                  // eslint-disable-next-line react/no-children-prop
+                  children={<BsSlashLg />} />
+                <Input
+                  type="text"
+                  value={path}
+                  onChange={(e) => setPath(e.target.value)}
+                  placeholder="Páginas separadas por vírgula Ex: /sobre,/blog,/contato"
+                  pl="35px!important"
+                  bg={bg}
+                  border={"none"} />
+              </InputGroup>
+              <Button type="submit">Adicionar</Button>
+            </Flex>
+          </FormControl>
         </form>
-        <List>
+        <Grid templateColumns={{ lg: pathsArray.length >= 2 ? "repeat(2,1fr)" : "repeat(1,1fr)" }} gap="4">
           {pathsArray.map((path, index) => (
-            <Box key={index} py={2}>
-              <ListItem py={2}>
-                {index + 1}- {path}
-                <Button onClick={() => handleDelete(index)}>Delete</Button>
-              </ListItem>
-              <Flex justifyContent="space-evenly" fontWeight={"bold"}>
+            <GridItem key={index}
+              border="1px"
+              borderColor={"#c9c9c9"}
+              borderRadius={"6px"}
+              display={"flex"}
+              flexDir={"column"}
+              gap="2"
+              p="4">
+              <Tag
+                variant='outline'
+                w="fit-content">
+                <TagLabel color={color}>{index + 1}- {path}</TagLabel>
+                <TagCloseButton onClick={() => handleDelete(index)} />
+              </Tag>
+              <Flex justifyContent="space-around" fontSize={"xs"} marginBottom={"-10px"}>
                 <Text>
                   Permitir
                 </Text>
@@ -209,14 +262,37 @@ export default function New() {
               <DualListBox
                 key={index}
                 options={options}
+                canFilter={true}
+                showOrderButtons={true}
                 selected={blockeds[index]} // Usando a seleção do componente específico
                 onChange={(newBlockedItems) => handleDualListBoxChange(index, newBlockedItems)}
               />
-            </Box>
+            </GridItem>
           ))}
-        </List>
-        <Input type="text" value={sitemap} onChange={(e) => setSiteMap(e.target.value)} placeholder="Sitemaps separados por vírgula" />
-        <Button onClick={generateResult}>Gerar</Button>
+        </Grid>
+        <FormControl isRequired>
+          <FormLabel
+            variant={"h6"}
+            color={"cinza"}>
+            Sitemaps
+          </FormLabel>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              h="full"
+              // eslint-disable-next-line react/no-children-prop
+              children={<MdHttp />}
+            />
+            <Input
+              type="text"
+              value={sitemap}
+              onChange={(e) => setSiteMap(e.target.value)}
+              placeholder="Sitemaps separados por vírgula Ex: https://www.webpeak.com.br/sitemap.xml/,https://www.webpeak.com.br/sitemap.xml2" pl="35px!important"
+              bg={bg}
+              border={"none"} />
+          </InputGroup>
+        </FormControl>
+        <Button onClick={handleResult}>Gerar</Button>
         {/* <pre>
           <code>
             {results.map((rule, index) => (
@@ -250,6 +326,6 @@ export default function New() {
           </InputGroup>
         </FormControl>
       </Container>
-    </>
+    </Style>
   )
 }
